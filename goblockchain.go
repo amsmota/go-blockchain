1df90b1a31cb1496574be93b6d7bf442d1f18483
Antonio Mota <anmota@deloitte.ie>
2022-03-30 15:27:13 +0100

    Go Blockchain: Proof Of Work

--- goblockchain.go
+++ goblockchain.go
@@ -9,10 +9,11 @@ import (
 	"time"
 )
 
+const MINING_DIFFICULTY = 3
 type Block struct {
+	timestamp    int64
 	nonce        int
 	previousHash [32]byte
-	timestamp    int64
 	transactions []*Transaction
 }
 
@@ -39,7 +40,6 @@ func (b *Block) Print() { // create method Print in Block
 
 func (b *Block) Hash() [32]byte {
 	m, _ := json.Marshal(b)
-	// fmt.Println(string(m))
 	sum := sha256.Sum256([]byte(m))
 	return sum
 }
@@ -98,6 +98,33 @@ func (bc *Blockchain) AddTransaction(sender string, recipient string, value floa
 	bc.transactionPool = append(bc.transactionPool, t)
 }
 
+func (bc *Blockchain) CopyTransactionPool() []*Transaction {
+	transactions := make([]*Transaction, 0)
+	for _, t := range bc.transactionPool {
+		transactions = append(transactions, 
+			NewTransaction(t.senderAddress, t.recipientAddress, t.value))
+	}
+	return transactions
+}
+
+func (bc *Blockchain) ValidProof(nonce int, previousHash [32]byte, transactions []*Transaction , dificulty int) bool{
+	zeros := strings.Repeat("0", dificulty)
+	guessBlock := Block{0, nonce, previousHash, transactions}
+	guessHash := fmt.Sprintf("%x", guessBlock.Hash())
+	// fmt.Println(guessHash)
+	return guessHash[:dificulty] == zeros
+}
+
+func (bc *Blockchain) ProofOfWork() int {
+	transactions := bc.CopyTransactionPool()
+	previousHash := bc.LastHash()
+	nonce := 0
+	for !bc.ValidProof(nonce, previousHash, transactions, MINING_DIFFICULTY){
+		nonce += 1
+	}
+	return nonce
+}
+
 type Transaction struct {
 	senderAddress    string
 	recipientAddress string
@@ -145,9 +172,13 @@ func init() {
 func main() {
 	bc := NewBlockchain()
 	bc.AddTransaction("aaa", "bbb", 10)
-	bc.CreateBlock(1, bc.LastHash())
+	nonce := bc.ProofOfWork()
+	bc.CreateBlock(nonce, bc.LastHash())
+	bc.Print()
+
 	bc.AddTransaction("ccc", "ddd", 20)
 	bc.AddTransaction("ddd", "eee", 30)
-	bc.CreateBlock(5, bc.LastHash())
+	nonce = bc.ProofOfWork()
+	bc.CreateBlock(nonce, bc.LastHash())
 	bc.Print()
 }
